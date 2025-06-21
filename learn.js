daaafa9185118eef17bae1b5c175ba0c5ca95266
anirudhkurnool <anirudhkurnool@gmail.com>
2025-06-21 13:11:46 +0530

    js started

---
+++ learn.js
@@ -0,0 +1,18 @@
+console.log("Hello, World!!!")
+
+function add(a, b){
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b 
+}
+
+function mul(a, b){
+    return a * b
+}
+
+function div(a, b){
+    assert(b != 0)
+    return a / b
+}
